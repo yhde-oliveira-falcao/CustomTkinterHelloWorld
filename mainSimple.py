d6aea84b8cf0a26b12570e50032e6d7eceec45bd
YHOFalcao <55162954+yhde-oliveira-falcao@users.noreply.github.com>
2023-03-25 23:51:00 -0400

    Add files via upload

---
+++ mainSimple.py
@@ -0,0 +1,18 @@
+import customtkinter
+
+customtkinter.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+
+app = customtkinter.CTk()
+app.geometry("300x100")
+app.title("Hello World example")
+
+
+frame_1 = customtkinter.CTkFrame(master=app)
+frame_1.pack(pady=20, padx=60, fill="both", expand=True)
+
+label_1 = customtkinter.CTkLabel(master=frame_1, text="Olá Mundo", justify=customtkinter.CENTER)
+label_1.pack(pady=10, padx=10)
+
+app.mainloop()
+
